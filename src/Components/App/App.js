f120e2df8adcc3d36d13005ceb3fae607feb096d
Tom Tobar <cocdetombomb@gmail.com>
2022-06-23 21:03:40 -0500

    add react app

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+
+import { Route, Routes } from 'react-router-dom';
+import Home from '../Home/Home';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route />
+        <Route />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
